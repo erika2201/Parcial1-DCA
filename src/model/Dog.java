85cf8b4a088a87629916d4a2c7da0068ce10460d
erika2201 <erikapapamija@gmail.com>
2021-04-25 17:25:00 -0500

    Try other way

--- src/model/Dog.java
+++ src/model/Dog.java
@@ -1,83 +1,42 @@
 package model;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import processing.core.PApplet;
 
-public class Dog {
-
-	private PApplet app;
-	private int id, age;
-	private String name, breed;
-	private Date birth; // I´m not sure with this
-	private String date;
-
-	public Dog(PApplet app, int id, String name, String breed, int age) {
-		this.app = app;
-		this.id = id;
-		this.name = name;
-		this.age = age;
-		this.breed = breed;
-		this.birth = birth;
-	}
-
-	public void drawInfo(int x) {
-
-		app.text("ID:" + id, x, 100);
-		app.text("Nombre:" + name, x, 120);
-		app.text("Edad:" + age, x, 142);
-		app.text("Raza:" + breed, x, 162);
-
-		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-		String date = format.format(this.birth);
-		app.text("B: " + date, x, 182);
-	}
-
+public class Dog implements Comparable<Dog> {
 	
-	public int compareTo(Dog nextDog) {
-		return this.id - nextDog.getId();
+	private PApplet app;
+	String name,breed;
+	int age,id;
+	
+	public Dog(int id, String name, int age, String breed,PApplet app) {
+		this.id=id;
+		this.name=name;
+		this.age=age;
+		this.breed=breed;
+		this.app=app;
 	}
+	public void drawAttributes(int x, int y) {
+		app.text(this.id, x, y);
+	
+		app.text(this.name, x, y);
 
-	//Getters
-	public int getId() {
-		return id;
+		app.text(this.age + " años", x, y);
+
+		app.text(this.breed, x, y);
 	}
-
 	public String getName() {
 		return name;
 	}
-	
-	public int getAge() {
-		return age;
-	}
-
 	public String getBreed() {
 		return breed;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
+	public int getAge() {
+		return age;
 	}
-
-	public void setRace(String breed) {
-		this.breed = breed;
+	public int getId() {
+		return id;
 	}
-
-	public Date getDate() {
-		return birth;
-	}
-
-	
-	//Setters
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public void setID(int iD) {
-		id = iD;
-	}
-
-	public void setDate(Date birth) {
-		this.birth = birth;
+	@Override
+	public int compareTo(Dog nextDog) {
+		return this.id - nextDog.getId();
 	}
 }
